ca70df29047039b3c6895ca81aff4f21c72f1202
Robyn Carnow <robynainsley2i@gmail.com>
2024-12-27 22:33:16 +0200

    About page changes

---
+++ src/assets/components/aboutInterests.jsx
@@ -0,0 +1,159 @@
+export default function AboutInterests() {
+  return (
+    <div className="relative rounded-lg bg-slate-900 p-2">
+      {/* From Uiverse.io by emmanuelh-dev   */}
+      <div className="relative flex text-center">
+        <div className="flex pl-3.5 pt-3">
+          <svg
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="-ml-0.5 mr-1.5 h-3 w-3 text-red-500/20"
+          >
+            <circle r="12" cy="12" cx="12"></circle>
+          </svg>
+          <svg
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="-ml-0.75 mr-1.5 h-3 w-3 text-yellow-500/20"
+          >
+            <circle r="12" cy="12" cx="12"></circle>
+          </svg>
+          <svg
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="-ml-0.75 mr-1.5 h-3 w-3 text-green-500/20"
+          >
+            <circle r="12" cy="12" cx="12"></circle>
+          </svg>
+        </div>
+        <span className="absolute inset-x-0 top-2 text-xs text-slate-500">
+          Interests of mine
+        </span>
+      </div>
+      <div className="mt-5 space-y-1.5 px-5 pb-10">
+        <p className="mt-4 font-mono text-xs font-normal tracking-wide text-violet-400">
+          <span className="text-slate-500">&lt;</span>
+          <span className="text-pink-400">AboutMe</span>
+          <span className="text-slate-500">&gt;</span>
+        </p>
+        <p className="ml-3 font-mono text-xs font-normal tracking-wide text-violet-400">
+          <span className="text-slate-500">&lt;</span>
+          <span className="text-pink-400">Personality</span>
+          <span className="text-slate-500">&gt;</span>
+
+          <p className="ml-4 font-mono text-xs font-normal tracking-wide text-violet-400">
+            <span className="text-slate-500">&lt;</span>
+            <span className="text-pink-400">Text</span>
+            <span className="text-slate-500">&gt;</span>
+            <span className="relative inline-block px-1 before:absolute before:block before:rounded before:bg-blue-500/10">
+              <span className="relative text-blue-400">
+                I am an open-minded and creative person, and enjoy learning
+                about the world of technology, the interest for which had
+                stemmed from curiosity of the new world.
+              </span>
+            </span>
+            <span className="text-slate-500">&lt;/</span>
+            <span className="text-pink-400">Text</span>
+            <span className="text-slate-500">&gt;</span>
+          </p>
+
+          <p className="ml-4 font-mono text-xs font-normal tracking-wide text-violet-400">
+            <span className="text-slate-500">&lt;</span>
+            <span className="text-pink-400">Text</span>
+            <span className="text-slate-500">&gt;</span>
+            <span className="relative inline-block px-1 before:absolute before:block before:rounded before:bg-blue-500/10">
+              <span className="relative text-blue-400">
+                Being organized and observant are skills of mine, combined with
+                the ability to adapt to my environment.
+              </span>
+            </span>
+            <span className="text-slate-500">&lt;/</span>
+            <span className="text-pink-400">Text</span>
+            <span className="text-slate-500">&gt;</span>
+          </p>
+          <span className="text-slate-500">&lt;/</span>
+          <span className="text-pink-400">Personality</span>
+          <span className="text-slate-500">&gt;</span>
+        </p>
+        <p className="ml-3 font-mono text-xs font-normal leading-4 tracking-wide text-violet-400">
+          <span className="text-slate-500">&lt;</span>
+          <span className="text-pink-400">Hobbies</span>
+          <span className="text-slate-500">&gt;</span>
+
+          <span className="relative inline-block px-1 before:absolute before:block before:rounded before:bg-blue-500/10">
+            <span className="relative text-blue-400">
+              In my free time, I enjoy reading books on self-improvement and
+              understanding how entrepreneurs think.
+            </span>
+          </span>
+
+          <span className="text-slate-500">&lt;/</span>
+          <span className="text-pink-400">Hobbies</span>
+          <span className="text-slate-500">&gt;</span>
+        </p>
+        <p className="ml-3 font-mono text-xs font-normal tracking-wide text-violet-400">
+          <span className="text-slate-500">&lt;</span>
+          <span className="text-pink-400">Aspirations</span>
+          <span className="ml-2 text-violet-400">
+            className<span className="text-slate-500">=</span>
+            <span className="relative inline-block px-1 before:absolute before:block before:rounded before:bg-blue-500/10">
+              <span className="relative text-blue-400">"mt-3"</span>
+            </span>
+          </span>
+          <span className="text-slate-500">&gt;</span>
+        </p>
+        <p className="ml-6 font-mono text-xs font-normal tracking-wide text-violet-400">
+          <span className="text-slate-500">&lt;</span>
+          <span className="text-pink-400">Text</span>
+          <span className="text-slate-500">&gt;</span>
+          <span className="text-blue-400"> Become a</span>
+          <span className="text-slate-500">&lt;</span>
+          <span className="text-pink-400">Bold</span>
+          <span className="text-slate-500">&gt;</span>
+          <span className="relative inline-block px-1 before:absolute before:block before:rounded before:bg-blue-500/10">
+            <span className="relative text-green-400">software engineer.</span>
+          </span>
+          <span className="text-slate-500">&lt;/</span>
+          <span className="text-pink-400">Bold</span>
+          <span className="text-slate-500">&gt;</span>
+
+          <span className="text-slate-500">&lt;/</span>
+          <span className="text-pink-400">Text</span>
+          <span className="text-slate-500">&gt;</span>
+        </p>
+        <p className="ml-6 font-mono text-xs font-normal tracking-wide text-violet-400">
+          <span className="text-slate-500">&lt;</span>
+          <span className="text-pink-400">Text</span>
+          <span className="text-slate-500">&gt;</span>
+          <span className="relative inline-block px-1 before:absolute before:block before:rounded before:bg-blue-500/10">
+            <span className="relative text-blue-400">
+              Build my own
+              <span className="text-slate-500">&lt;/</span>
+              <span className="text-pink-400">Bold</span>
+              <span className="text-slate-500">&gt;</span>
+              <span className="relative inline-block px-1 before:absolute before:block before:rounded before:bg-blue-500/10">
+                <span className="relative text-green-400">brand.</span>
+              </span>
+              <span className="text-slate-500">&lt;/</span>
+              <span className="text-pink-400">Bold</span>
+              <span className="text-slate-500">&gt;</span>
+            </span>
+          </span>
+          <span className="text-slate-500">&lt;/</span>
+          <span className="text-pink-400">Text</span>
+          <span className="text-slate-500">&gt;</span>
+        </p>
+        <p className="ml-3 font-mono text-xs font-normal tracking-wide text-violet-400">
+          <span className="text-slate-500">&lt;/</span>
+          <span className="text-pink-400">Aspirations</span>
+          <span className="text-slate-500">&gt;</span>
+        </p>
+        <p className="font-mono text-xs font-normal tracking-wide text-violet-400">
+          <span className="text-slate-500">&lt;/</span>
+          <span className="text-pink-400">AboutMe</span>
+          <span className="text-slate-500">&gt;</span>
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/assets/pages/About.jsx
+++ src/assets/pages/About.jsx
@@ -1,7 +1,67 @@
+import React, { useState, useEffect } from "react";
+
+import AboutInterests from "../components/aboutInterests";
+
 export default function About() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   return (
-    <>
-      <h1>About page</h1>
-    </>
+    <div className="page_container" id="about">
+      {/* <h1>A little about me.</h1>
+      <div className="about_hero">
+        <div className="col-item">
+          <img
+            loading="lazy"
+            src="https://robynainsley21.github.io/images/vue-portfolio-images/robyn-about.jpg"
+            alt="about_img"
+          />
+        </div>
+        <div className="col-item about_text">
+          <p>
+            
+          </p>
+
+        </div>
+      </div> */}
+
+      <AboutInterests />
+
+
+
+
+      <div className="about_body">
+        <h2 data-aos="fade-up">Interests of mine</h2>
+        <p className="fine-text text-center mb-5">Hover to view details</p>
+
+        {/* <div className="interest-container row mb-5">
+          {data.about[1].interests.map((item) => {
+            return (
+              <InterestCard
+                title={item.title}
+                details={item.details}
+                img={item.img_url}
+              />
+            );
+          })}
+        </div> */}
+
+        {/* <div className="about_mindmap">
+<div className="grid_item"></div>
+<div className="grid_item"></div>
+<div className="grid_item"></div>
+<div className="grid_item"></div>
+<div className="grid_item middle_item">middle</div>
+<div className="grid_item"></div>
+<div className="grid_item"></div>
+<div className="grid_item"></div>
+<div className="grid_item"></div>
+</div> */}
+
+        
+
+      </div>
+      <div>{/* <Achievements data={data} loading={loading} /> */}</div>
+    </div>
   );
 }
