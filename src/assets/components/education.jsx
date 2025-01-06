84a08d21b99339f9c2471f53902b961207f00fe3
Robyn Carnow <robynainsley2i@gmail.com>
2025-01-06 13:26:05 +0200

    Add experiences

---
+++ src/assets/components/education.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+
+import ButtonComp from "./Button";
+export default function Education() {
+    const [data, setData] = useState([
+        {
+          id: 1,
+          edu_name: "Life Choices Academy",
+          date: "April 2024 - September 2024",
+          details: "Graduated Fullstack Developer",
+          site: "https://lifechoices.co.za/life-choices-academy/",
+          img_url:
+            "https://robynainsley21.github.io/images/personal-portfolio/life-choices.jpg",
+          skills: ["Vue.js", "Node.js", "MySQL", "Teamwork"],
+        },
+        {
+          id: 2,
+          edu_name: "CodeSpace",
+          date: "February 2023 - August 2023",
+          details: "Graduated Software Developer",
+          site: "https://www.codespace.co.za/",
+          img_url:
+            "https://robynainsley21.github.io/images/personal-portfolio/codespace.jpeg",
+          skills: ["React.js", "Git", "HTML", "CSS", "JavaScript"],
+        },
+        {
+          id: 3,
+          edu_name: "Tuscany Glen Secondary High",
+          date: "2014 - 2018",
+          details: "Deputy Head Girl, Second-best Achiever",
+          site: "https://www.facebook.com/tuscanyglenhighschool/",
+          img_url:
+            "https://robynainsley21.github.io/images/personal-portfolio/tuscany-secondary.jpg",
+          skills: ["Communication", "Leadership", "Teamwork"],
+        },
+      ]);
+      return (
+        <div>
+          <h1>Education</h1>
+          <div className="education_container">
+            {data.map((edu) => {
+              return (
+                <div className="edu_card" key={edu.id}>
+                  <img src={edu.img_url} alt="edu_img" />
+                  <div className="edu_text">
+                    <h5>{edu.edu_name}</h5>
+                    <p>{edu.date}</p>
+                    <p>{edu.details}</p>
+                    <ButtonComp text="Visit" />
+                  </div>
+                  <div className="edu_skill_box">
+                    <h6>Skills learned:</h6>
+                    <div className="edu_skills">
+                      {edu.skills.map((skill) => {
+                        return <div className="skill_pill ">{skill}</div>;
+                      })}
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      );
+}
---
+++ src/assets/components/experiences.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import ButtonComp from "./Button";
+
+export default function Experience() {
+    const [data, setData] = useState([
+        {
+            "id": 1,
+            "company": "LC Studio",
+            "duration": "September 2024 - Present",
+            "title": "Intern",
+            "site": "https://lcstudio.co.za/",
+            "img_url": "https://robynainsley21.github.io/images/personal-portfolio/lc_studioza_logo.jpeg"
+          },
+          {
+            "id": 2,
+            "title": "Sales Assistant",
+            "company": "Poppies Home Bakes",
+            "duration": "August 2022 - April 2023",
+            "img_url": "https://robynainsley21.github.io/images/personal-portfolio/poppies-img.jpg",
+            "site": "https://www.facebook.com/PoppiesLangverwacht/"
+          }
+    ]);
+    return (
+        <div>
+            <h1>Experiences</h1>
+            <div className="exp_container">
+                {data.map((item) => (
+                    <div className="exp_card" key={item.id}>
+                        <img src={item.img_url} alt="exp_img" />
+                        <div className="exp_text">
+                            <h5>{item.title}</h5>
+                            <p>{item.company}</p>
+                            <p>{item.duration}</p>
+                        </div>
+                        <div className="exp_btns">
+                        <ButtonComp text="Details"/>
+                        <ButtonComp text="Visit"/>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
